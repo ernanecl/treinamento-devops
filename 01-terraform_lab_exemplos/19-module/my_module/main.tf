541bcde955437fd5de224c7bede3b1a66284e46e
ernane <ernanecl@gmail.com>
2021-10-14 14:46:53 -0300

    crete module vpc and ec2

--- 01-terraform_lab_exemplos/19-module/my_module/main.tf
+++ 01-terraform_lab_exemplos/19-module/my_module/main.tf
@@ -1,3 +1,13 @@
 resource "aws" {
   region = "sa-east-1"
 }
+
+module "vpc_subnet" {
+  source = "./create_network
+  nome = "VPC nome"
+}
+
+module "aws_instance"{
+  source = "./create_network"
+  nome = "EC2 nome"
+}
