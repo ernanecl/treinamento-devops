0ba22a271b12563d88c19926e75c09ff5491592d
ernane <ernanecl@gmail.com>
2021-10-14 15:13:25 -0300

    creating and setting instance

---
+++ 01-terraform_lab_exemplos/19-module/lab_module/resources_aws/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "sa-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami                     = data.aws_ami.ubuntu.id
+  instance_type           = "t2.micro"
+  key_name                = "key-dev-ernane-aws" # key chave publica cadastrada na AWS 
+  subnet_id               =  aws_subnet.my_subnet_1a.id # vincula a subnet direto e gera o IP automático
+  private_ip              = "192.168.10.30"
+  vpc_security_group_ids  = [
+    "${aws_security_group.allow_ssh_terraform.id}",
+  ]
+
+  root_block_device {
+    volume_size = "8"
+    volume_type = "gp2"
+    encrypted = true
+    kms_key_id = "ad51fbbd-816c-4ee5-a1bd-c9218b648507"
+    #delete_on_termination = true
+  }
+  
+  tags = {
+    Name = "Maquina terraform - ernane"
+  }
+}
+
+resource "aws_eip" "example" {
+  vpc = true
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.web.id
+  allocation_id = aws_eip.example.id
+}
+
+# terraform refresh para mostrar o ssh
+
+output "aws_instance_e_ssh" {
+  value = [
+    aws_instance.web.public_ip,
+    "ssh -i id_rsa_itau_treinamento ubuntu@${aws_instance.web.public_dns}"
+  ]
+}
