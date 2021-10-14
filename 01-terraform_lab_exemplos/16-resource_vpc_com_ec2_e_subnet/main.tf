717c59e8fb1c778a33f9fdab8d7c09d5dbcd2a80
ernanecl <ernanecl@gmail.com>
2021-10-14 14:28:36 +0000

    create vpc, subnet and ec2 with terraform code

--- 01-terraform_lab_exemplos/16-resource_vpc_com_ec2_e_subnet/main.tf
+++ 01-terraform_lab_exemplos/16-resource_vpc_com_ec2_e_subnet/main.tf
@@ -1,32 +1,58 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "sa-east-1"
 }
+
+
+
+
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  key_name      = "Itau_treinamento" # key chave publica cadastrada na AWS 
-  # subnet_id        =  aws_subnet.my_subnet.id # vincula a subnet direto e gera o IP automático
-  tags = {
-    Name = "Maquina para testar VPC"
+  key_name      = "key-dev-ernane-aws"                # key chave publica cadastrada na AWS
+  subnet_id     = aws_subnet.my_subnet.id # vincula a subnet direto e gera o IP automático
+  #private_ip = "10.16.0.101"
+  # vpc_security_group_ids = ["${aws_security_group.allow_ssh1.id}"]
+
+  root_block_device {
+    volume_size = "8"
+    volume_type = "gp2"
+    encrypted   = true
+    kms_key_id  = "ad51fbbd-816c-4ee5-a1bd-c9218b648507" #"f48a0432-3f72-4888-9b31-8bdf1c121a4c"
+    #delete_on_termination = true
   }
 
-  network_interface {
-    network_interface_id = aws_network_interface.my_subnet.id # vincula a subnet com ip fixo
-    device_index         = 0 # DeviceIndex - The network interface's position in the attachment order. For example, the first attached network interface has a DeviceIndex of 0. 
+
+
+
+  tags = {
+    Name = "Maquina para testar VPC - ernane"
   }
 }
+
+
+
 
 resource "aws_instance" "web2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
-  key_name      = "Itau_treinamento"
-  tags = {
-    Name = "Maquina2 para testar VPC"
+  key_name      = "key-dev-ernane-aws"                  # key chave publica cadastrada na AWS
+  subnet_id     = aws_subnet.my_subnet_b.id # vincula a subnet direto e gera o IP automático
+  #private_ip = "10.16.1.100"
+  # vpc_security_group_ids = ["${aws_security_group.allow_ssh1.id}"]
+
+  root_block_device {
+    volume_size = "8"
+    volume_type = "gp2"
+    encrypted   = true
+    kms_key_id  = "ad51fbbd-816c-4ee5-a1bd-c9218b648507" #"f48a0432-3f72-4888-9b31-8bdf1c121a4c"
+    #delete_on_termination = true
   }
 
-  network_interface {
-    network_interface_id = aws_network_interface.my_subnet_b.id # vincula a subnet com ip fixo
-    device_index         = 0
+
+
+
+  tags = {
+    Name = "Maquina2 para testar VPC - ernane"
   }
 }
--- 01-terraform_lab_exemplos/16-resource_vpc_com_ec2_e_subnet/vpc.tf
+++ 01-terraform_lab_exemplos/16-resource_vpc_com_ec2_e_subnet/vpc.tf
@@ -1,59 +1,59 @@
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
+  cidr_block = "10.0.2.0/24"
 
   tags = {
-    Name = "tf-lab-danilo-vpc"
+    Name = "tf-lab-ernane-vpc"
   }
 }
 
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-east-1a"
+  cidr_block        = "10.0.2.0/27"
+  availability_zone = "sa-east-1a"
 
   tags = {
-    Name = "tf-lab-danilo-subnet"
+    Name = "tf-lab-ernane-subnet-1a"
   }
 }
 
 resource "aws_subnet" "my_subnet_b" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.20.0/24"
-  availability_zone = "us-east-1b"
+  cidr_block        = "10.0.2.32/27"
+  availability_zone = "sa-east-1b"
 
   tags = {
-    Name = "tf-lab-danilo-subnet_b"
+    Name = "tf-lab-ernane-subnet_1b"
   }
 }
 
 resource "aws_subnet" "my_subnet_c" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.30.0/24"
-  availability_zone = "us-east-1c"
+  cidr_block        = "10.0.2.64/27"
+  availability_zone = "sa-east-1c"
 
   tags = {
-    Name = "tf-lab-danilo-subnet_c"
+    Name = "tf-lab-ernane-subnet_c"
   }
 }
 
 
 resource "aws_network_interface" "my_subnet" {
   subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.101"] # IP definido para instancia
+  private_ips = ["10.0.2.5"] # IP definido para instancia
   # security_groups = ["${aws_security_group.allow_ssh1.id}"]
 
   tags = {
-    Name = "primary_network_interface my_subnet"
+    Name = "primary_network_interface_ernane_my_subnet_a"
   }
 }
 
 
 resource "aws_network_interface" "my_subnet_b" {
   subnet_id   = aws_subnet.my_subnet_b.id
-  private_ips = ["172.16.20.100"] # IP definido para instancia
+  private_ips = ["10.0.2.36"] # IP definido para instancia
   # security_groups = ["${aws_security_group.allow_ssh1.id}"]
 
   tags = {
-    Name = "primary_network_interface my_subnet_b"
+    Name = "primary_network_interface_ernane_my_subnet_b"
   }
 }
