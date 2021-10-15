6980ff9a312cda336e266eb55fd052f1caafc4bd
ernanecl <ernanecl@gmail.com>
2021-10-15 14:31:34 +0000

    create instance for Ansible with Terraform

--- 02-ansible/terraform/main.tf
+++ 02-ansible/terraform/main.tf
@@ -3,14 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "ami-054a31f1b3bf90920"
   instance_type = "t2.micro"
   key_name      = "key-dev-ernane-aws"                # key chave publica cadastrada na AWS
 
-  tags = {
-    "Name" = "Ansible and Terraform - Ernane"
-  }
-  # subnet_id     = aws_subnet.my_subnet.id # vincula a subnet direto e gera o IP automático
+  subnet_id     = aws_subnet.my_subnet_c.id # vincula a subnet direto e gera o IP automático
   # private_ip = "10.16.0.101"
   # vpc_security_group_ids = ["${aws_security_group.allow_ssh1.id}"]
 
@@ -19,5 +16,9 @@ resource "aws_instance" "web" {
     volume_type = "gp2"
     encrypted   = true
     kms_key_id  = "ad51fbbd-816c-4ee5-a1bd-c9218b648507"
+  }
+
+  tags = {
+    Name = "Ansible and Terraform - Ernane"
   }
 }
--- 02-ansible/terraform/vpc.tf
+++ 02-ansible/terraform/vpc.tf
@@ -1,6 +1,14 @@
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "tf-ansible-ernane-vpc"
+  }
+}
+
 resource "aws_subnet" "my_subnet_c" {
   vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.2.64/27"
+  cidr_block        = "10.0.1.0/27"
   availability_zone = "sa-east-1c"
 
   tags = {
@@ -9,12 +17,12 @@ resource "aws_subnet" "my_subnet_c" {
 }
 
 
-resource "aws_network_interface" "my_subnet" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["10.0.2.5"] # IP definido para instancia
-  # security_groups = ["${aws_security_group.allow_ssh1.id}"]
+#resource "aws_network_interface" "my_subnet_c" {
+ # subnet_id   = aws_subnet.my_subnet.id
+ # private_ips = ["10.0.1.5"] # IP definido para instancia
+ # security_groups = ["${aws_security_group.allow_ssh1.id}"]
 
-  tags = {
-    Name = "primary_network_interface_ernane_my_subnet_a"
-  }
-}
+ # tags = {
+  #  Name = "primary_network_interface_ernane_my_subnet_a"
+  #}
+#}
